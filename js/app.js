3046481c1988cd690dcd4ccdf406f22269d59dc7
unknown <raedmhmd11244@gmail.com>
2019-12-01 14:53:22 +0300

    Add Modal, Add Movement Player & Enamy,Add Score

--- js/app.js
+++ js/app.js
@@ -21,14 +21,24 @@
 //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 //};
 
+let panel = document.getElementsByClassName('panel')[0];
+let Score = document.getElementById('score');
+let backgroundColor = document.getElementsByTagName('body')[0];
+let scoreCalculate = 0;
+
 class Enemy {
     constructor(x, y) {
         this.x = x
         this.y = y
         this.sprite = 'images/enemy-bug.png';
-        this.speed = 5;
+        this.speed = 150;
     }
-    update() { }
+    update(dt) {
+        this.x += this.speed * dt;
+        if(this.x > 510){
+            this.x = -100;
+        }
+     }
     render() { ctx.drawImage(Resources.get(this.sprite), this.x, this.y); }
 
 }
@@ -40,25 +50,39 @@ class Player {
         this.y = y
         this.sprite = 'images/char-boy.png';
     }
+   
     handleInput(dir){
-        if (dir == "left"  && this.x > 0)
+        if (dir === "left"  && this.x > 0)
         {
             this.x -= 100;
         }
-        else if (dir == "up" && this.y > -10)
+        else if (dir === "up" && this.y > 20)
         {
-            this.y -= 100;
+            this.y -= 90;
         }
-        else if (dir == "right" && this.x <= 300)
+        else if (dir === "right" && this.x <= 300)
         {
             this.x += 100;
         }
-        else (dir == "down"&& this.y <= 400)
+        else if  (dir === "down" && this.y <= 400)
         {
-            this.y += 100;
+            this.y += 90;
         }
     }
-    update() { }
+    update() { 
+        if(this.y <= 0 ){
+           scoreCalculate += 50;
+           score.textContent = scoreCalculate;
+            this.x = 200;
+            this.y = 420;
+
+        }
+        if ( scoreCalculate === 100){
+            backgroundColor.style.background = "#2e2e2e";
+
+            panel.style.display = "block";
+        }
+    }
     render() { ctx.drawImage(Resources.get(this.sprite), this.x, this.y); }
 }
 // This class requires an update(), render() and
@@ -66,7 +90,7 @@ class Player {
 
 
 // Now instantiate your objects.
-let player = new Player(200, 410);
+let player = new Player(200, 420);
 // Place all enemy objects in an array called allEnemies
 let enemy1 = new Enemy(100, 60);
 let enemy2 = new Enemy(200, 150);
